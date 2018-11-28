77328c09ba2551d2bd1f90710000970b3268df83
Halima95 <Halimaalshahrani@gmail.com>
2018-11-28 14:29:03 +0300

    Post Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
-Rails.application.routes.draw do
-  get 'dialogues/index'
-  get 'dialogues/show'
-  get 'dialogues/new'
-  get 'dialogues/edit'
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/edit'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+ Rails.application.routes.draw do
+ resources :posts, :dialogues, :items
   devise_for :admins
   root "pages#home"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
