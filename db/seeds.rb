f61b32e5d409d7b41636777873328cbcd955d593
NouraAlhadlaq <noura6.1994@gmail.com>
2018-11-28 16:02:32 +0300

    Items controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.destroy_all
+
+plastic_bag = Item.create(name: "Plastic Bag", image: "https://cdn2.iconfinder.com/data/icons/waste-management-and-recycling/64/plastic_bag-512.png")
