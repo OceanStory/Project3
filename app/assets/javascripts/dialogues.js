6e9c16d5fd7a2f315229386036181918f29c361b
leanaAlq <leana.a.q@gmail.com>
2018-11-30 22:52:25 +0300

    ajax, random items, and atheer's background

--- app/assets/javascripts/dialogues.js
+++ app/assets/javascripts/dialogues.js
@@ -1,2 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function responseToJSON(response) {
+  return response.json();
+}
+function displayDialogues(data) {
+  console.log(data);
+  dialogesList = data
+    .map(function(dialogue) {
+      return `<div class="dialogue" id="dialogue-${
+        dialogue.id
+      }"><p>${dialogue.content}</p><a href="/posts/1">Read more</a></div>`;
+    })
+    .join("");
+  const DialogueDiv = document.querySelector("#dialogues");
+  console.log(dialogesList);
+  DialogueDiv.innerHTML = `${dialogesList}`;
+}
+function dispalyItems(data) {
+  console.log(data);
+}
+function getDialogues() {
+  fetch("/dialogues", {
+    headers: { "Content-Type": "application/json", Accept: "application/json" }
+  })
+    .then(responseToJSON)
+    .then(displayDialogues);
+}
+getDialogues();
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @items }
+    end
   end
 
   def show
