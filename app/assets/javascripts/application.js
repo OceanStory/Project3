6dd08e35e41f5338a6881bac87c9d488662bbce8
NouraAlhadlaq <noura6.1994@gmail.com>
2018-12-02 10:53:09 +0300

    whale and item animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,14 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-var colors = new Array(
-  [0, 100, 100],
-  [0, 40, 100],
-  [0, 35, 100],
-  [0, 50, 100],
-  [0, 20, 100],
-  [0, 12, 0]
-);
+var colors = new Array([ 0, 100, 100 ], [ 0, 40, 100 ], [ 0, 35, 100 ], [ 0, 50, 100 ], [ 0, 20, 100 ], [ 0, 12, 0 ]);
 
 var step = 0;
 //color table indices for:
@@ -29,59 +22,83 @@ var step = 0;
 // next color left
 // current color right
 // next color right
-var colorIndices = [0, 1, 2, 3];
+var colorIndices = [ 0, 1, 2, 3 ];
 
 //transition speed
 var gradientSpeed = 0.0;
 
 function updateGradient() {
-  if ($ === undefined) return;
+	if ($ === undefined) return;
 
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
+	var c0_0 = colors[colorIndices[0]];
+	var c0_1 = colors[colorIndices[1]];
+	var c1_0 = colors[colorIndices[2]];
+	var c1_1 = colors[colorIndices[3]];
 
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+	var istep = 1 - step;
+	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+	var color1 = 'rgb(' + r1 + ',' + g1 + ',' + b1 + ')';
 
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+	var color2 = 'rgb(' + r2 + ',' + g2 + ',' + b2 + ')';
 
-  $("#gradient")
-    .css({
-      background:
-        "-webkit-gradient(linear, left top, right top, from(" +
-        color1 +
-        "), to(" +
-        color2 +
-        "))"
-    })
-    .css({
-      background:
-        "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
-    });
+	$('#gradient')
+		.css({
+			background: '-webkit-gradient(linear, left top, right top, from(' + color1 + '), to(' + color2 + '))'
+		})
+		.css({
+			background: '-moz-linear-gradient(left, ' + color1 + ' 0%, ' + color2 + ' 100%)'
+		});
 
-  step += gradientSpeed;
-  if (step >= 1) {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
+	step += gradientSpeed;
+	if (step >= 1) {
+		step %= 1;
+		colorIndices[0] = colorIndices[1];
+		colorIndices[2] = colorIndices[3];
 
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] =
-      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
-      colors.length;
-    colorIndices[3] =
-      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
-      colors.length;
-  }
+		//pick two new target color indices
+		//do not pick the same as the current one
+		colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+		colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+	}
 }
 
 setInterval(updateGradient, 50);
+
+$.fn.animateTo = function(cssProps, time = 100, easing = 'linear') {
+	// Save the value of this (this represents the element that the method was called upon)
+
+	const el = this;
+
+	// Create a new Promise instance of Promise. Promises receive an "executor" function (a function that takes time). And the executor function will receive a resolve function (so that .then will be called), and a reject function (so that .catch will be called)
+
+	const p = new Promise(function(resolve, reject) {
+		// Select the element, and call the built-in animate method
+		// The animate method receives an object of CSS to change, a time to take, an easing function and an on-complete function (called at the end of the animation)
+		// We are using resolve to call the .then method
+
+		$(el).animate(cssProps, time, easing, () => {
+			resolve($(el));
+		});
+	});
+
+	// Return the promise, so that we can use .then
+
+	return p;
+};
+
+$('#Item').animateTo({ right: '150px', top: '200px', left: '300px' }, 8000).then(function(el) {
+	el.css('transform', 'rotate(20deg)');
+});
+
+setTimeout(function() {
+	$('#d-1').show();
+}, 2000);
+setTimeout(function() {
+	$('#d-1').hide();
+	$('#d-2').show();
+}, 3000);
