d72eee59d740f81d3bc48f3e97e598731553d82e
leanaAlq <leana.a.q@gmail.com>
2018-12-03 22:37:23 +0300

    bubbles working on start page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :posts, :dialogues, :items
   devise_for :admins
-  root "pages#home"
+  resource :pages do
+    member do
+      get "home"
+    end
+    get "start"
+  end
+  root "pages#start"
 end
